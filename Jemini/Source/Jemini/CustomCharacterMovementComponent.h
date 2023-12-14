abd785d8c160b70b148afe6fabda37b0e1721a55
elliottHong <tjdgus3174@gmail.com>
2023-12-14 14:38:00 -0800

    !Fixed problem with climbing on tilted wall

--- Jemini/Source/Jemini/CustomCharacterMovementComponent.h
+++ Jemini/Source/Jemini/CustomCharacterMovementComponent.h
@@ -24,23 +24,33 @@ class JEMINI_API UCustomCharacterMovementComponent : public UCharacterMovementCo
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditDefaultsOnly)
-	float WallAttractionForce = 200.f;
-	UPROPERTY(EditDefaultsOnly)
-	float WallJumpOffForce = 300.f;
-	UPROPERTY(EditDefaultsOnly)
-	float WallJumpForce = 400.f;
-	UPROPERTY(EditDefaultsOnly)
-	float MaxClimbSpeed = 300.f;
-	UPROPERTY(EditDefaultsOnly)
-	float BrakingDecelerationClimbing = 1000.f;
-	UPROPERTY(EditDefaultsOnly)
-	float ClimbReachDistance = 200.f;
-	UPROPERTY(EditDefaultsOnly)
-	float MaxSprintSpeed = 600.f;
+	UPROPERTY(EditDefaultsOnly) float WallAttractionForce = 200.f;
+	UPROPERTY(EditDefaultsOnly) float WallJumpOffForce = 300.f;
+	UPROPERTY(EditDefaultsOnly) float WallJumpForce = 400.f;
+	UPROPERTY(EditDefaultsOnly) float MaxClimbSpeed = 300.f;
+	UPROPERTY(EditDefaultsOnly) float BrakingDecelerationClimbing = 1000.f;
+	UPROPERTY(EditDefaultsOnly) float ClimbReachDistance = 200.f;
+	UPROPERTY(EditDefaultsOnly) float MaxSprintSpeed = 600.f;
 
-	UPROPERTY(Transient)
-	AJeminiCharacter* MyCharacterOwner;
+	UPROPERTY(EditDefaultsOnly) float MantleMaxDistance = 200;
+	UPROPERTY(EditDefaultsOnly) float MantleReachHeight = 50;
+	UPROPERTY(EditDefaultsOnly) float MinMantleDepth = 30;
+	UPROPERTY(EditDefaultsOnly) float MantleMinWallSteepnessAngle = 75;
+	UPROPERTY(EditDefaultsOnly) float MantleMaxSurfaceAngle = 40;
+	UPROPERTY(EditDefaultsOnly) float MantleMaxAlignmentAngle = 45;
+	UPROPERTY(EditDefaultsOnly) UAnimMontage* TallMantleMontage;
+	UPROPERTY(EditDefaultsOnly) UAnimMontage* TransitionTallMantleMontage;
+	UPROPERTY(EditDefaultsOnly) UAnimMontage* ProxyTallMantleMontage;
+	UPROPERTY(EditDefaultsOnly) UAnimMontage* ShortMantleMontage;
+	UPROPERTY(EditDefaultsOnly) UAnimMontage* TransitionShortMantleMontage;
+	UPROPERTY(EditDefaultsOnly) UAnimMontage* ProxyShortMantleMontage;
+
+	UPROPERTY(Transient) AJeminiCharacter* MyCharacterOwner;
+	TSharedPtr<FRootMotionSource_MoveToForce> TransitionRMS;
+	FString TransitionName;
+	UPROPERTY(Transient) UAnimMontage* TransitionQueuedMontage;
+	float TransitionQueuedMontageSpeed;
+	int TransitionRMS_ID;
 
 	bool bWantsToClimb;
 	bool bWantsToSprint;
@@ -65,7 +75,12 @@ protected:
 private:
 	bool TryClimb();
 	void PhysClimb(float deltaTime, int32 Iterations);
-	
+
+	bool TryMantle();
+	FVector GetMantleStartLocation(FHitResult FrontHit, FHitResult SurfaceHit, bool bTallMantle) const;
+
+	float CapR() const;
+	float CapHH() const;
 public:
 	UFUNCTION(BlueprintCallable)
 	void ClimbPressed();
